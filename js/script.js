bc5330a2382b4812bdc72056164bee31884a1cee
Sadiq Shaikh <49910780+Sadiq98@users.noreply.github.com>
2024-01-24 00:03:05 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 
+
+function populateCard(cardElement, productData) {
+    const titleElement = cardElement.querySelector('.card-title');
+    const priceElement = cardElement.querySelector('.price');
+    const imageElement = cardElement.querySelector('.card-img-top');
+
+    titleElement.innerHTML = productData.title;
+    priceElement.innerHTML = productData.price;
+    imageElement.src = productData.image;
+
+}
+
+function getProduct() {
+    // Make API call
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            // Find all card elements
+            const cardElements = document.querySelectorAll('.card,.thumbnail');
+
+            // Populate each card with data
+            cardElements.forEach((cardElement, index) => {
+                populateCard(cardElement, data[index]);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching products.', error);
+        });
+}
+
+window.addEventListener('load', getProduct);
+
+
