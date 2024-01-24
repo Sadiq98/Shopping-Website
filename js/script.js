f7034d435a9068115646179581283870bba5267a
Sadiq Shaikh <49910780+Sadiq98@users.noreply.github.com>
2024-01-24 23:41:03 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
 
 
+
+
+
 function populateCard(cardElement, productData) {
     const titleElement = cardElement.querySelector('.card-title');
     const priceElement = cardElement.querySelector('.price');
+    // const categryElement = cardElement.querySelector('.category');
+
     const imageElement = cardElement.querySelector('.card-img-top');
 
     titleElement.innerHTML = productData.title;
     priceElement.innerHTML = productData.price;
-    imageElement.src = productData.image;
-
+    // categryElement.innerHTML=productData.category;
+    imageElement.src = productData.thumbnail;
 }
 
 function getProduct() {
     // Make API call
-    fetch('https://fakestoreapi.com/products')
+    // fetch('https://fakestoreapi.com/products')
+        fetch('https://dummyjson.com/products')
+
         .then(response => response.json())
         .then(data => {
             console.log(data);
 
             // Find all card elements
-            const cardElements = document.querySelectorAll('.card,.thumbnail');
+            const cardElements = document.querySelectorAll('.card');
 
             // Populate each card with data
             cardElements.forEach((cardElement, index) => {
-                populateCard(cardElement, data[index]);
+                populateCard(cardElement, data.products[index]);
             });
         })
         .catch(error => {
